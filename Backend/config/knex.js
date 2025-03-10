4735017f2b026df376daacf383add177a2e82eba
navidzare7775 <navidzare7775@gmail.com>
2025-03-10 10:47:42 +0330

    Knex_config

--- Backend/config/knex.js
+++ Backend/config/knex.js
@@ -1,12 +1,17 @@
 import knex from "knex";
+
+const connectionConfig = process.env.POSTGRES_URL
+  ? { connectionString: process.env.POSTGRES_URL }
+  : {
+      host: process.env.PGHOST || "127.0.0.1",
+      port: process.env.DB_PORT || 5432,
+      user: process.env.PGUSER || "postgres",
+      password: process.env.PGPASSWORD || "test",
+      database: process.env.PGDATABASE || "smart-brain",
+    };
+
 const knexConfig = knex({
   client: "pg",
-  connection: {
-    host: process.env.PGHOST || "127.0.0.1",
-    port: process.env.DATABASE_URL || 5432,
-    user: process.env.PGUSER || "postgres",
-    password: process.env.PGPASSWORD || "test",
-    database: process.env.PGDATABASE || "smart-brain",
-  },
+  connection: Object.assign({}, connectionConfig),
 });
 export default knexConfig;
